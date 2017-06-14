4efb1cb44de6481673e2002bc886156f1ce7a3d1
bogdan radacina <bogdan.radacina@gmail.com>
2017-06-14 21:30:07 +1000

    fixing up vue display issues in admin inventory edit

--- static/inventory-items-list.js
+++ static/inventory-items-list.js
@@ -5,10 +5,8 @@ Vue.component('inventory-items-list', {
     <div>
         <inventory-item v-for="item in items" :key="item.SKU" v-bind:item="item" />
     </div>`,
+
     data: function() {
-        return {items: [
-            {Title: "12312", SKU:"1234"},
-            {Title: "dddd", SKU: "343535"}
-        ]
-    };}
+        return {items: window.inventoryItems};
+    }
 });
